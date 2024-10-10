31d31b3af9747d47ede334fb60b40a6d96eae5cf
Tiago Pace <tiagopace.dev@gmail.com>
2024-10-10 19:09:15 -0300

    chore: set up shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
-   <div>Hello World.</div>
-  );
+  <div className="flex h-screen items-center justify-center">
+    <Button>Click</Button>
+  </div>
+  )
 }
